e7a05874275735d295e72f15c7a0b73c0ae1f0c8
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2026-02-05 12:59:32 +0600

    'ha'

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/posts', [PostController::class,'index'])->name('posts.index');
+Route::post('/posts', [PostController::class,'store'])->name('posts.store');
+Route::get('/posts/{id}', [PostController::class,'show'])->name('posts.show');
